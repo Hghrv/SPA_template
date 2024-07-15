5811dc7a937fc41306b2cde00e45472b5875d748
Hghrv <77743082+Hghrv@users.noreply.github.com>
2024-07-15 17:46:18 +0100

    Add files via upload

---
+++ App.css
@@ -0,0 +1,133 @@
+
+
+body {
+  font-family: Lato, sans-serif;
+  background-color: lightblue;
+  margin: 5%;
+}
+
+h1 {
+  padding: 1% 5%;
+  color: lavender;
+  text-align: center;
+  background-color: green;
+  border: 5px solid lightgreen;
+  text-transform: uppercase;
+  letter-spacing: 0.3em;
+  font-weight: bold;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px lightyellow;
+}
+
+ol {
+  color: #e06363;
+  font-weight: bold;
+  box-shadow: 2px 2px 5px lightblue;
+  border-radius: 15px;
+  padding: 5% 10%;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+  margin-bottom: 10%;
+  line-height: 1.5;
+}
+
+
+footer p {
+  text-align: center;
+  background-color: white;
+  color: grey;
+  font-family: Lato, sans-serif;
+  border-radius: 10px;
+}
+
+h4 span {
+  text-align: center;
+  margin: 3% 3% 3% 3%;
+}
+
+h3 {
+  text-transform: uppercase;
+}
+p {
+  color: White;
+}
+
+#text {
+  float: right;
+}
+.logo {
+  text-align: center;
+  padding: 2.5% 1.15%;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px lightblue;
+}
+
+.rndrect {
+  border-radius: 15px;
+}
+#rrct {
+  border-radius: 5px;
+  text-align: center;
+}
+
+#pjct {
+  color: grey;
+  text-align: center;
+  padding: 5% 5% 10% 0%;
+}
+
+#shadow {
+  box-shadow: 2px 2px 5px lightblue;
+  border-radius: 5px;
+}
+#shad {
+  text-align: center;
+  padding: 10% 3% 3% 3%;
+}
+.bounce {
+  animation-name: bounce;
+  animation-duration: 12s;
+  animation-fill-mode: both;
+  box-shadow: 2px 2px 5px lightblue;
+  animation-iteration-count: Infinite;
+  }
+
+@keyframes bounce {
+  0%,
+  17%,
+  55%,
+  80%,
+  100% {
+    transform: translateY(3px);
+    
+  }
+  
+  
+  42% {
+    transform: translateY(-12px);
+  }
+  
+  
+  70% {
+    transform: translateY(-4px);
+  }
+}
+
+@keyframes glow{
+  from {
+    text-shadow: 0 0 10px lightyellow, 0 0 20px lightyellow, 0 0 25px lightgreen, 0 0 30px lightgreen, 0 0 40px white, 0 0 45px red, 0 0 50px lightyellow;
+  }
+  to {
+    text-shadow: 0 0 20px lightblue, 0 0 20px lightblue, 0 0 25px blue, 0 0 30px darkblue, 0 0 40px blue, 0 0 45px lightblue, 0 0 50px lightblue;
+  }
+}
+
+.glow {animation: glow 15s ease-in-out infinite alternate
+}
+
+#Logo {
+  
+}
